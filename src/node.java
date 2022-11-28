9631a3438aba144fe6b6415292ce817313a23205
Neil Varia <neilvaria@gmail.com>
2022-11-28 09:32:54 +0000

    first commit

---
+++ src/node.java
@@ -0,0 +1,11 @@
+public class node {
+    int jug1;
+    int jug2;
+    int jug3;
+
+    public node(int jug1, int jug2, int jug3){
+        this.jug1 = jug1;
+        this.jug2 = jug2;
+        this.jug3 = jug3;
+    }
+}
